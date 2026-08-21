60dff7fd715a7ab55fe33998b5a6bcf7443027d4
VISHWA1508 <mkvishwa1507@gmail.com>
2026-08-21 19:54:20 +0530

    o

--- src/data/questionPapers/pgdm1/file16.js
+++ src/data/questionPapers/pgdm1/file16.js
@@ -8,7 +8,7 @@ export const PGDM1_FILE11 = [
     id: "PGDM1_F11_Q01",
     category: "Finance",
     question: "A security is expected to pay ₹10,000 exactly one year from today. If the discount rate is 10%, what is its present value?",
-    answer: "₹9,090.91",
+    answer: "9090.91",
     acceptedAnswers: ["₹9,090.91", "9090.91", "9090.9", "₹9090.91","9,090.9"]
   }
 ];
